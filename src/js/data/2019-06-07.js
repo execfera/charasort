f0b36420f59a51c215419a6c500f23f5e0fbd6bf
Amir Fuhaira <amirfuhairah@gmail.com>
2019-10-04 21:47:52 +0800

    update dataset to oct 2019

--- src/js/data/2019-06-07.js
+++ src/js/data/2019-06-07.js
@@ -1,4 +1,4 @@
-dataSetVersion = "2019-05-15"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSetVersion = "2019-06-07"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
