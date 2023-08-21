fae64e713d3df86a4517a89a6b5baae563296942
etermellis <millyfluffy77@gmail.com>
2023-08-22 00:10:59 +0500

    aa8

--- src/js/data.js
+++ src/js/data.js
@@ -15,7 +15,7 @@ const dataSet = {};
  * 
  * @example '2018-02-20'
 */
-let dataSetVersion = '';
+let dataSetVersion = '2023-08-21';
 
 /**
  * Image root, will be appended to the start of every image URL.
