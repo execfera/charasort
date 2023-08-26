194d984aaf8cc0d11ef35a5609e96caa5fd1c968
etermellis <millyfluffy77@gmail.com>
2023-08-26 17:59:42 +0500

    aa9

--- src/js/data.js
+++ src/js/data.js
@@ -20,4 +20,4 @@ let dataSetVersion = '2023-08-21';
 /**
  * Image root, will be appended to the start of every image URL.
  */
-const imageRoot = 'https://i.imgur.com/';
+const imageRoot = 'src/assets/chars/';
